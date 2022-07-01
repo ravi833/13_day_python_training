942d38484d5f6736f0372073889ce049b1972695
ravi833 <107167422+ravi833@users.noreply.github.com>
2022-07-01 11:42:31 +0530

    Add files via upload

---
+++ range.py
@@ -0,0 +1,37 @@
+# for i in range(0,2):
+#     print(i)  #0
+#             #    1
+
+
+# c=1,2,3,4,5,6
+# for i in (c):
+#     print(i,end=' ')   #1 2 3 4 5 6 
+
+# for i in range(2,3,1):
+#     print(i)   #2
+ 
+# for i in range(10,4,-3):
+#     print(i)    #10,7
+
+# for v in range(34,57,5):
+#     print(v) # 34,39,44,49,54
+
+
+# a={1:234,2:345}
+# for c in (a):
+#     print(c)  #1 2
+
+# for c in range (456,567,100):
+#     print(c)  #456 556
+
+
+
+# a=[2,'raju',2.5]    #2
+# for c in (a):      #raju
+#     print(c,sep='  ')   # 2.5
+
+a={1,24,'rt34',5.6}
+for c in(a):
+    print(c,end='  ')
+
+   
